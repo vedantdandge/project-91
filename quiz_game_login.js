982ab4cf732701bd55ebce0be2cd964e25ad49dc
vedantdandge <76774280+vedantdandge@users.noreply.github.com>
2021-02-20 15:00:19 +0530

    Add files via upload

---
+++ quiz_game_login.js
@@ -0,0 +1,7 @@
+function addUser(){
+    var player1_name=document.getElementById("player1_name_input").value;
+    var player2_name=document.getElementById("player2_name_input").value;
+    localStorage.setItem("player1_name", number_1);
+    localStorage.setItem("player2_name", number_2);
+    window.location="game_page.html";
+}
